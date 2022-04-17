51e6f38dc76990a1eb6f37127fc55b73a0eb7d20
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2022-04-17 21:07:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import "./App.css";
+import Aside from "./components/Aside/Aside";
+import MyRoutes from "./components/MyRoutes";
+import NavBar from "./components/NavBar/NavBar";
 import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
+      <NavBar />
+      <div className="grid-col-2--20-80">
+        <Aside />
+        <MyRoutes />
+      </div>
     </div>
   );
 }
